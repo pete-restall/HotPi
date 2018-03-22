b48bb019253c4403d87bacafce224cc54b48dad2
pete-restall <pete@restall.net>
2018-03-22 15:44:46 +0000

    Basic watchdog component.

--- src/software/HotPi/Reflow/Ui/Angular/src/app/page-container/page-container.module.ts
+++ src/software/HotPi/Reflow/Ui/Angular/src/app/page-container/page-container.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 
 import { RealtimeTemperatureModule } from '../realtime-temperature/realtime-temperature.module';
+import { WatchdogModule } from '../watchdog/watchdog.module';
 import { PageContainerComponent } from './page-container.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { PageContainerComponent } from './page-container.component';
     RouterModule,
     NbLayoutModule,
     NbSidebarModule,
-    RealtimeTemperatureModule
+    RealtimeTemperatureModule,
+    WatchdogModule
   ],
   exports: [PageContainerComponent],
   providers: [NbSidebarService],
