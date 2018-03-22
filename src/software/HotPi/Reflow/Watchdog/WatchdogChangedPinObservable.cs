4f231c63f8262a59d4e4249c1c8541eb8d1c59b1
pete-restall <pete@restall.net>
2018-03-22 14:15:21 +0000

    Renamed WatchdogExpired to WatchdogChanged as it can be used to capture both states with one hub.

--- src/software/HotPi/Reflow/Watchdog/WatchdogChangedPinObservable.cs
+++ src/software/HotPi/Reflow/Watchdog/WatchdogChangedPinObservable.cs
@@ -4,13 +4,13 @@ using Restall.HotPi.Reflow.Gpio;
 
 namespace Restall.HotPi.Reflow.Watchdog
 {
-	public class WatchdogTimerExpiredPinObservable : IObserve<GpioPinChanged>
+	public class WatchdogChangedPinObservable : IObserve<GpioPinChanged>
 	{
 		private static readonly ProcessorPin WatchdogExpiredPin = ConnectorPin.P1Pin19.ToProcessor();
 
-		private readonly IObserve<WatchdogTimerExpired> observer;
+		private readonly IObserve<WatchdogChanged> observer;
 
-		public WatchdogTimerExpiredPinObservable(IObserve<WatchdogTimerExpired> observer)
+		public WatchdogChangedPinObservable(IObserve<WatchdogChanged> observer)
 		{
 			this.observer = observer;
 		}
@@ -20,7 +20,7 @@ namespace Restall.HotPi.Reflow.Watchdog
 			if (observed.Pin != WatchdogExpiredPin)
 				return;
 
-			this.observer.Observe(new WatchdogTimerExpired(DateTime.Now));
+			this.observer.Observe(new WatchdogChanged(DateTime.Now, observed.Active));
 		}
 	}
 }
