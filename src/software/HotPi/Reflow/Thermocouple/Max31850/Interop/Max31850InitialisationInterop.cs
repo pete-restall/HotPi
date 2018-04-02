4bbeaf735704b1bcbf9224503a8de45ff916c0bf
pete-restall <pete@restall.net>
2018-04-02 10:00:34 +0100

    Specifying calling convention explicitly, and other minor tweaks to get the interop working successfully.

--- src/software/HotPi/Reflow/Thermocouple/Max31850/Interop/Max31850InitialisationInterop.cs
+++ src/software/HotPi/Reflow/Thermocouple/Max31850/Interop/Max31850InitialisationInterop.cs
@@ -10,7 +10,7 @@ namespace Restall.HotPi.Reflow.Thermocouple.Max31850.Interop
 			Max31850ErrorInterop.ThrowExceptionIfResultCodeIsError(result, nameof(Max31850Initialise));
 		}
 
-		[DllImport(Max31850Interop.NativeLibraryName, EntryPoint = "max31850Initialise")]
+		[DllImport(Max31850Interop.NativeLibraryName, EntryPoint = "max31850Initialise", CallingConvention = CallingConvention.Cdecl)]
 		private static extern int Max31850Initialise();
 
 		public void Shutdown()
@@ -18,7 +18,7 @@ namespace Restall.HotPi.Reflow.Thermocouple.Max31850.Interop
 			Max31850Shutdown();
 		}
 
-		[DllImport(Max31850Interop.NativeLibraryName, EntryPoint = "max31850Shutdown")]
+		[DllImport(Max31850Interop.NativeLibraryName, EntryPoint = "max31850Shutdown", CallingConvention = CallingConvention.Cdecl)]
 		private static extern int Max31850Shutdown();
 	}
 }
